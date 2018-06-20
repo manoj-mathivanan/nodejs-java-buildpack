854d081627e668aea117d9e000db453705047e43
Manoj <ma.manoj@gmail.com>
2018-06-20 09:18:35 +0530

    Update binaries_java.sh

--- lib/binaries_java.sh
+++ lib/binaries_java.sh
@@ -1,7 +1,7 @@
 install_java() {
   local version="$1"
   local dir="$2"
-  
+  echo pwd
   local download_url="http://download.oracle.com/otn-pub/java/jdk/10.0.1+10/fb4372174a714e6b8c52526dc134031e/jdk-10.0.1_linux-x64_bin.tar.gz"
   echo "Downloading JAVA [$download_url]"
   curl -v -j -k -L -H "Cookie: oraclelicense=accept-securebackup-cookie" "$download_url" -o /home/vcap/tmp/java.tar.gz || (echo "Unable to download java; does it exist?" && false)
