648b246226f5ff2ed47853dd15851a93fe7b2083
Manoj <ma.manoj@gmail.com>
2018-06-20 09:16:17 +0530

    Update binaries_java.sh

--- lib/binaries_java.sh
+++ lib/binaries_java.sh
@@ -4,20 +4,17 @@ install_java() {
   
   local download_url="http://download.oracle.com/otn-pub/java/jdk/10.0.1+10/fb4372174a714e6b8c52526dc134031e/jdk-10.0.1_linux-x64_bin.tar.gz"
   echo "Downloading JAVA [$download_url]"
-  curl -v -j -k -L -H "Cookie: oraclelicense=accept-securebackup-cookie" "$download_url" -o /tmp/java.tar.gz || (echo "Unable to download java; does it exist?" && false)
+  curl -v -j -k -L -H "Cookie: oraclelicense=accept-securebackup-cookie" "$download_url" -o /home/vcap/tmp/java.tar.gz || (echo "Unable to download java; does it exist?" && false)
   echo "Download complete!"
 
   echo "Installing JAVA"
-  mkdir /tmp/jdk
-  mkdir $dir
-  tar xzf /tmp/java.tar.gz -C /tmp/jdk
-  rm -rf $dir/*
-  mv /tmp/jdk/jdk-10.0.1/* $dir
-  chmod +x $dir/bin
-  JAVA_HOME=$dir
-  PATH=$dir/bin:$PATH
+  rm -rf /home/vcap/tmp/jdk
+  mkdir /home/vcap/tmp/jdk
+  tar xzf /home/vcap/tmp/java.tar.gz -C /home/vcap/tmp/jdk
+  chmod +x /home/vcap/tmp/jdk/bin
+  JAVA_HOME=/home/vcap/tmp/jdk
+  PATH=/home/vcap/tmp/jdk/bin:$PATH
   echo $PATH
-  echo $dir
-  ls $dir/bin
+  ls /home/vcap/tmp/jdk/bin
   echo "Installation complete!"	
 }
